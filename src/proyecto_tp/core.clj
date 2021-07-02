4c01a8f5ea50c7fa91efca75a5d1ea5dc18d3175
Maxi Romero <mromero@itaptec.com.ar>
2021-07-01 21:38:35 -0300

    Agrego enunciado

--- src/proyecto_tp/core.clj
+++ src/proyecto_tp/core.clj
@@ -1,25 +1,7 @@
 (ns proyecto-tp.core
-  (:gen-class))
-
-(declare ejemplo)
-(declare es-el-doble?)
-(declare spy)
+  (:gen-class)
+  (:require [proyecto-tp.enunciado :refer :all]))
 
 (defn -main
   "Ejemplo de Proyecto en Clojure"
-  [& args] (ejemplo))
-
-(defn ejemplo []
-  (let [mensaje-1 (do (print "Ingrese un numero: ") (flush))
-        n1 (read)
-        mensaje-2 (do (print "Ingrese el doble del numero anterior: ") (flush))
-        n2 (read)
-        salida (println (str "Ud." (if (es-el-doble? n1 n2) " " " no ") "sabe calcular!"))]
-    (do (print "Presione Enter...") (flush) (read-line) (read-line) 'Chau!)))
-
-(defn es-el-doble? [a b]
-  (= (* 2 a) b))
-
-(defn spy
-  ([x] (do (prn x) x))
-  ([msg x] (do (print msg) (print ": ") (prn x) x)))
+  [& args] (driver-loop))
--- test/proyecto_tp/core_test.clj
+++ test/proyecto_tp/core_test.clj
@@ -4,5 +4,4 @@
 
 (deftest es-el-doble?-test
   (testing "Prueba de la funcion: es-el-doble?"
-    (is (= true (es-el-doble? 4 8)))
-    (is (= false (es-el-doble? 4 7)))))
+    (is (= true (= 4 4)))))
