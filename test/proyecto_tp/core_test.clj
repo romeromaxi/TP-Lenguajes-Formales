3840ab07ed48dc9129b6d4a672bf51414e67153c
Maxi Romero <mromero@itaptec.com.ar>
2021-07-05 12:03:22 -0300

    Agrego cadena?

--- test/proyecto_tp/core_test.clj
+++ test/proyecto_tp/core_test.clj
@@ -1,7 +1,22 @@
 (ns proyecto-tp.core-test
   (:require [clojure.test :refer :all]
-            [proyecto-tp.core :refer :all]))
+            [proyecto-tp.core :refer :all]
+            [proyecto-tp.enunciado :refer :all]))
 
-(deftest es-el-doble?-test
-  (testing "Prueba de la funcion: es-el-doble?"
-    (is (= true (= 4 4)))))
+(deftest a-mayusculas-salvo-strings-test
+  (testing "Prueba de la funcion: a-mayusculas-salvo-strings"
+    (is (= "" (a-mayusculas-salvo-strings nil)))
+    (is (= "  CONST Y = 2;" (a-mayusculas-salvo-strings "  const Y = 2;")))
+    (is (= "  WRITELN ('Se ingresa un valor, se muestra su doble.');" (a-mayusculas-salvo-strings "  writeln ('Se ingresa un valor, se muestra su doble.');")))
+  )
+)
+
+(deftest cadena?-test
+  (testing "Prueba de la funcion: cadena?"
+    (is (= false (cadena? nil)))
+    (is (= true (cadena? "'Hola'")))
+    (is (= false (cadena? "Hola")))
+    (is (= false (cadena? "'Hola")))
+    (is (= false (cadena? 'Hola)))
+  )
+)
